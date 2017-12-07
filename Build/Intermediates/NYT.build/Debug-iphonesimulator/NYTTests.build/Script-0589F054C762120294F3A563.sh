d65366aa0db1daf79e39b66ebc8b6c72a4484f58
Istvan Bagi <istvan.bagi@misys.com>
2017-12-07 17:45:50 +0100

    Stop tracking of build dir.

--- Build/Intermediates/NYT.build/Debug-iphonesimulator/NYTTests.build/Script-0589F054C762120294F3A563.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-"${SRCROOT}/Pods/Target Support Files/Pods-NYTTests/Pods-NYTTests-frameworks.sh"
-
--- Build/Intermediates/NYT.build/Debug-iphonesimulator/NYTUITests.build/Script-E7FAB5F61395878AE50C59FD.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-"${SRCROOT}/Pods/Target Support Files/Pods-NYTUITests/Pods-NYTUITests-frameworks.sh"
-
